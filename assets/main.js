fe669def36a6d8a664b38cf8a24407ca484d8a3a
JakeRoberts99 <jake.s.roberts99@gmail.com>
2021-12-30 21:55:21 -0700

    added basic file structure and add some psuedo code to js and some html elements have been added

---
+++ assets/main.js
@@ -0,0 +1,3 @@
+// When I click "Start", The welcome form hide and Question 1 is displayed
+// When I click "Submit", Question 1 hides and Question 2 displays ect..
+// When Question 1 displays start a timer countdown (setInterval)
