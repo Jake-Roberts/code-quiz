3846b89c55d495a9598ee86771071ac28846f295
JakeRoberts99 <jake.s.roberts99@gmail.com>
2021-12-31 00:13:36 -0700

    redid the html to be simpler and now it has psuedo code in there aswell

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,4 @@
-// When I click "Start", The welcome form hide and Question 1 is displayed
+// Get a timer to start
+// When I click "Start", The welcome form hides and Question 1 is displayed
 // When I click "Submit", Question 1 hides and Question 2 displays ect..
 // When Question 1 displays start a timer countdown (setInterval)
